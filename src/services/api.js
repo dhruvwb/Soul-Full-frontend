fcc41b3798c276c0377d8b8fd5c899afe73ccce0
Dhruvpal <palsahab427@gmail.com>
2026-03-22 12:04:15 +0530

    Switch to environment variables for API base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'https://soul-full-backend.onrender.com/api';
+const API_BASE = process.env.REACT_APP_API_BASE || 'https://soul-full-backend.onrender.com/api';
 const ROOT_BASE = API_BASE.replace(/\/api\/?$/, '');
 
 const api = axios.create({ baseURL: API_BASE });
